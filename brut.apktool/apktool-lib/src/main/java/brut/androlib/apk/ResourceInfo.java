03ebac732811948c44b2f08b6828a1fe045f33c4
XdXdXd <13690362+AkkaXdXd@users.noreply.github.com>
2024-11-01 19:08:25 +0800

    Update ResourceInfo.java

--- brut.apktool/apktool-lib/src/main/java/brut/androlib/apk/ResourceInfo.java
+++ brut.apktool/apktool-lib/src/main/java/brut/androlib/apk/ResourceInfo.java
@@ -1,1 +1,21 @@
+package brut.androlib.apk;
 
+public class ResourceInfo implements YamlSerializable {
+    public String renameResourcesPackage;
+
+    @Override
+    public void readItem(YamlReader reader) {
+        YamlLine line = reader.getLine();
+        switch (line.getKey()) {
+            case "renameResourcesPackage": {
+                renameResourcesPackage = line.getValue();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void write(YamlWriter writer) {
+        writer.writeString("renameResourcesPackage", renameResourcesPackage);
+    }
+}
